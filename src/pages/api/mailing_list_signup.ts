c73db69caadb34a15f48a7585ecc5a475f6539fa
Toby Liu <ybot1122@gmail.com>
2025-02-06 10:20:18 -0800

    Form submission

---
+++ src/pages/api/mailing_list_signup.ts
@@ -0,0 +1,25 @@
+export const prerender = false; // Not needed in 'server' mode
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = async ({ request }) => {
+  const data = await request.formData();
+  const name = data.get("name");
+  const email = data.get("email");
+  // Validate the data - you'll probably want to do more than this
+  if (!name || !email) {
+    return new Response(
+      JSON.stringify({
+        message: "Missing required fields",
+      }),
+      { status: 400 }
+    );
+  }
+  // Do something with the data, then return a success response
+  console.log(name, email);
+  return new Response(
+    JSON.stringify({
+      message: "Success!",
+    }),
+    { status: 200 }
+  );
+};
