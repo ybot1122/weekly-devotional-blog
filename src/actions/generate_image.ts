fa9dd9f0c88c967c178008e20569d68463479d79
Toby Liu <ybot1122@gmail.com>
2025-06-16 12:33:53 -0700

    generate image

---
+++ src/actions/generate_image.ts
@@ -0,0 +1,41 @@
+import OpenAI from "openai";
+const OPENAI_API_KEY = import.meta.env.OPENAI_API_KEY;
+const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
+
+import { defineAction } from "astro:actions";
+import { z } from "astro:schema";
+import { getCollection } from "astro:content";
+
+export const generate_image = {
+  generateImage: defineAction({
+    input: z.object({
+      post_id: z.string(),
+    }),
+    handler: async (input) => {
+      const post = (await getCollection("blog")).filter(
+        (a) => a.id === input.post_id
+      )[0];
+
+      try {
+        const response = await openai.responses.create({
+          model: "gpt-4.1-mini",
+          input: `Generate an image for this blog post: ${post.data.description}`,
+          tools: [{ type: "image_generation" }],
+        });
+        const imageData = response.output
+          .filter((output) => output.type === "image_generation_call")
+          .map((output) => output.result);
+
+        if (imageData.length > 0) {
+          const imageBase64 = imageData[0];
+
+          return imageBase64;
+        }
+      } catch (e) {
+        console.error(e);
+      }
+
+      return "error";
+    },
+  }),
+};
