a52a3a6aa3a18512c40a8963c27364ab5b8d44cf
Toby Liu <ybot1122@gmail.com>
2025-06-23 13:45:25 -0700

    follow up buttons

--- src/actions/generate_image.ts
+++ src/actions/generate_image.ts
@@ -7,26 +7,48 @@ import { z } from "astro:schema";
 import { getCollection } from "astro:content";
 import { getSession } from "auth-astro/server";
 
+export const follow_ups = ["More Realistic", "More Detailed", "More Colorful"];
+
 export const generate_image = {
   generateImage: defineAction({
     input: z.object({
-      post_id: z.string(),
+      post_id: z.string().optional(),
+      follow_up_id: z.number().optional(),
+      previous_response_id: z.string().optional(),
     }),
     handler: async (input, ctx) => {
       const session = await getSession(ctx.request);
 
-      if (session?.user?.email !== "liutoby92@gmail.com") {
-        return "error";
+      if (
+        !input.post_id &&
+        !input.follow_up_id &&
+        !input.previous_response_id
+      ) {
+        throw new Error(
+          "post_id or follow_up_id+previous_response_id is required"
+        );
       }
 
-      const post = (await getCollection("blog")).filter(
-        (a) => a.id === input.post_id
-      )[0];
+      if (session?.user?.email !== "liutoby92@gmail.com") {
+        throw new Error("error");
+      }
+
+      let prompt = "";
+
+      if (input.post_id) {
+        const post = (await getCollection("blog")).filter(
+          (a) => a.id === input.post_id
+        )[0];
+        prompt = `Genreate an image for this blog post: ${post.data.description}`;
+      } else {
+        prompt = `Now make it ${follow_ups[input.follow_up_id!]}`;
+      }
 
       try {
         const response = await openai.responses.create({
           model: "gpt-4.1-mini",
-          input: `Generate an image for this blog post: ${post.data.description}`,
+          input: prompt,
+          previous_response_id: input.previous_response_id,
           tools: [
             {
               type: "image_generation",
@@ -36,6 +58,7 @@ export const generate_image = {
             },
           ],
         });
+
         const imageData = response.output
           .filter((output) => output.type === "image_generation_call")
           .map((output) => output.result);
@@ -43,13 +66,12 @@ export const generate_image = {
         if (imageData.length > 0) {
           const imageBase64 = imageData[0];
 
-          return imageBase64;
+          return { imageBase64, reponse_id: response.id };
         }
       } catch (e) {
         console.error(e);
+        throw new Error("Failed to generate image");
       }
-
-      return "error";
     },
   }),
 };
